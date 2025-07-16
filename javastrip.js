e006e58c4fc4bbb7b38c772321fa0fcc53abe2d6
pasidu10 <punnyanamda.thero@gmail.com>
2025-07-16 23:04:22 +0530

    Update and rename js.sve to javastrip.js

--- javastrip.js
+++ javastrip.js
@@ -17,3 +17,20 @@ async function saveToFirebase(name, email, message) {
     alert("Something went wrong!");
   }
 }
+// Initialize Firebase
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyXXX...",
+  authDomain: "pasiya-md.firebaseapp.com",
+  projectId: "pasiya-md",
+  storageBucket: "pasiya-md.appspot.com",
+  messagingSenderId: "XXXXXXX",
+  appId: "1:XXXXXX:web:XXXXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
