c212194b2107f12bd7587891ac87c16afb709045
pasidu10 <punnyanamda.thero@gmail.com>
2025-10-27 17:10:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ setInterval(() => {
   count++;
   counter.textContent = count.toLocaleString();
 }, 2000); // Every 1 seconds increase
+
+const statusText = document.getElementById('statusText');
+const messages = [
+  "Deploying bot...",
+  "Connecting to PASIYA-MD Server...",
+  "Setting up instance...",
+  "Finalizing deployment...",
+  "Bot deployed successfully!",
+  "Waiting for next instance..."
+];
+let index = 0;
+
+setInterval(() => {
+  statusText.textContent = messages[index];
+  index = (index + 1) % messages.length;
+}, 2000);
