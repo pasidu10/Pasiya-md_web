b33db6d15e9d3b0f905616ce46d67ad2f987440e
pasidu10 <punnyanamda.thero@gmail.com>
2025-10-27 17:09:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let count = 799000;
+let count = 799900;
 const counter = document.getElementById('counter');
 
 setInterval(() => {
   count++;
   counter.textContent = count.toLocaleString();
-}, 2000); // Every 2 seconds increase
+}, 2000); // Every 1 seconds increase
